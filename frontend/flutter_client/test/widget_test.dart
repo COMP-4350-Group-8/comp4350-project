381a8b8662b9b5ffa7a1526462f5bd272667aee5
Von-Seth <93102925+Von-Seth@users.noreply.github.com>
2024-10-09 06:37:27 -0500

    Update widget_test.dart

--- frontend/flutter_client/test/widget_test.dart
+++ frontend/flutter_client/test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_client/main.dart';
-import 'package:go_router/src/route.dart';
 
 void main() {
   testWidgets('Is Nav Bar Navigating', (WidgetTester tester) async {
