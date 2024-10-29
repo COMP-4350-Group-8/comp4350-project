d91df3f2fad5be1d41294291350ddabfdb9de667
OwenHny <76971990+OwenHny@users.noreply.github.com>
2024-10-28 20:16:28 -0500

    Create Course tests

---
+++ backend/Tests/CourseTests.cs
@@ -0,0 +1,22 @@
+﻿using SailMapper.Data;
+using SailMapper.Services;
+
+namespace Tests
+{
+    public class CourseTests : IDisposable
+    {
+        private readonly CourseService _service;
+        private readonly SailDBContext _dbContext;
+
+        public CourseTests()
+        {
+            _dbContext = CreateDB.InitalizeDB();
+            _service = new CourseService(_dbContext);
+        }
+
+        public void Dispose()
+        {
+            CreateDB.DeleteTempDB(_dbContext);
+        }
+    }
+}
