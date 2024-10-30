6dc22a6bcea2e716438be5c860f937af5e3fa952
Shaun <parapla1@myumanitoba.ca>
2024-10-30 15:55:35 -0500

    Create React Website with create course functionality

---
+++ frontend/react/src/components/map/Gmap.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "../ui/Card.jsx"
+
+export default  function Gmap(props)  {
+
+    // eslint-disable-next-line react/prop-types
+    const mapSrc = `https://maps.google.com/maps?q=${props.latitude},${props.longitude}&z=15&output=embed`;
+    return (
+        <div>
+            <iframe
+                title="Google Map"
+                width="340"
+                height="270"
+                frameBorder="0"
+                style={{border: 0}}
+                src={mapSrc}
+            ></iframe>
+        </div>
+    );
+}
--- frontend/react/src/pages/CreateRace.jsx
+++ frontend/react/src/pages/CreateRace.jsx
@@ -1,63 +1,24 @@
-import React, {useState} from "react";
 import "./CreateRace.css"
+import RaceForm from "../components/forms/RaceForm.jsx";
 
 export default  function CreateRace()  {
-    const [newNote, setNewNote] = useState("");
-    const [notes, setNotes] = useState([]);
 
-    function handleSubmit(e){
-        e.preventDefault();
-
-        setNotes((currentNotes) => {
-                return[
-                    ...currentNotes, {id: crypto.randomUUID(), title: newNote}
-                ]
+    //Enter the URL in the '' fo the fetch function
+    function addCourseHandler(courseData) {
+        fetch(
+            '',
+            {
+                method: 'POST',
+                body: JSON.stringify(courseData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
             }
-        )
-
-        setNewNote("")
+        );
     }
-
-    function deleteNote(id){
-        setNotes((currentNotes) => {
-            return currentNotes.filter(note => note.id !== id)
-        })
-    }
-
 
     return (
-        <>
-            <div className="create-race-header">
-                <h1> Create Race </h1>
-            </div>
-            <form onSubmit={handleSubmit} className="create-race-form">
-                <div className="form-row">
-                    <label htmlFor = "note" className= "new-note-header">Notes For Race</label>
-                    <input
-                        className="new-note-input"
-                        value = {newNote}
-                        onChange={(e) => setNewNote(e.target.value)}
-                        type="text"
-                        id="note"
-                        placeholder="Enter new note"/>
-                </div>
-                <button className= "submit-button" >Add Note</button>
-            </form>
-            <h2 className= "notes-header">NOTES</h2>
-            <ul className= "notes-list">
-                {notes.length === 0 && "No Notes"}
-                {notes.map(note =>{
-                    return <li key = {note.id}>
-                        <label>
-                            {note.title}
-                        </label>
-                        <button onClick={() => deleteNote(note.id)}
-                                className="delete-note">
-                            Delete
-                        </button>
-                    </li>
-                })}
-            </ul>
-        </>
+        <RaceForm  onAddCourse ={addCourseHandler}/>
     );
+
 }
