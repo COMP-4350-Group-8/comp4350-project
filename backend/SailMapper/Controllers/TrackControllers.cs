55d8dbf97973a2cf50307458b642c52f1a4ec510
nurlanua <nurlanua@myumanitoba.ca>
2024-10-31 02:53:31 -0500

    Added Track documentation

--- backend/SailMapper/Controllers/TrackControllers.cs
+++ backend/SailMapper/Controllers/TrackControllers.cs
@@ -15,7 +15,37 @@ namespace SailMapper.Controllers
             trackService = new TrackService(dbContext);
         }
 
-        // to create a track
+        /// <summary>
+        /// Creates a track
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        /// POST /track
+        /// 
+        /// {  
+        /// 
+        ///     "id": int,                  // id of the track
+        ///     
+        ///     "boatId": int,              // id of the boat track belongs to
+        ///     
+        ///     "raceId": int,              // id of the race the boat was tracked
+        ///     
+        ///     "started": dateTime,        // UTC start time of the race 
+        ///     
+        ///     "finished": dateTime,       // UTC end time of the race
+        ///     
+        ///     "distance": float,          // distance covered by the boat
+        ///     
+        ///     "gpxData": string,          // XML file from the GPS tracker
+        ///     
+        ///     "currentRating": int        // current rating of the boat
+        ///     
+        /// }
+        /// </remarks>
+        /// <response code="201">Track created successfully.</response>
+        /// <response code="400">If any of the required fields are missing or invalid.</response>
+        /// <response code="500">If there is an internal server error.</response>
         [HttpPost("")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -35,7 +65,12 @@ namespace SailMapper.Controllers
             return Problem();
         }
 
-        // to get all tracks of a race
+        /// <summary>
+        /// Gets all the tracks for the specific race
+        /// </summary>
+        /// <response code="200">Action successfull.</response>
+        /// <response code="500">If there is an internal server error.</response>
+        /// <param name="id"></param>
         [HttpGet("race/{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -46,7 +81,13 @@ namespace SailMapper.Controllers
 
         }
 
-        // get a specific tracks
+        /// <summary>
+        /// Gets a specific track
+        /// </summary>
+        /// <response code="200">Action successfull.</response>
+        /// <response code="404">Track not found.</response>
+        /// <response code="500">If there is an internal server error.</response>
+        /// <param name="id"></param>
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -62,7 +103,39 @@ namespace SailMapper.Controllers
         }
 
 
-        //updating a track
+        /// <summary>
+        /// Updates a track
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        /// PUT /track/{id}
+        /// 
+        /// {  
+        /// 
+        ///     "id": int,                  // id of the track
+        ///     
+        ///     "boatId": int,              // id of the boat track belongs to
+        ///     
+        ///     "raceId": int,              // id of the race the boat was tracked
+        ///     
+        ///     "started": dateTime,        // UTC start time of the race 
+        ///     
+        ///     "finished": dateTime,       // UTC end time of the race
+        ///     
+        ///     "distance": float,          // distance covered by the boat
+        ///     
+        ///     "gpxData": string,          // XML file from the GPS tracker
+        ///     
+        ///     "currentRating": int        // current rating of the boat
+        ///     
+        /// }
+        /// </remarks>
+        /// <response code="200">Track updated successfully.</response>
+        /// <response code="400">If any of the required fields are missing or invalid.</response>
+        /// <response code="404">Track not found.</response>
+        /// <response code="500">If there is an internal server error.</response>
+        /// <param name="id"></param>
         [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -89,7 +162,13 @@ namespace SailMapper.Controllers
         }
 
 
-        // getting gpx file
+        /// <summary>
+        /// Gets a GPX file
+        /// </summary>
+        /// <response code="200">Action successfull.</response>
+        /// <response code="404">Track not found.</response>
+        /// <response code="500">If there is an internal server error.</response>
+        /// <param name="id"></param>
         [HttpGet("{id}/gpx")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
