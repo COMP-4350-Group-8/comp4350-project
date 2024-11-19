073668c987807d4c474f29b18b08e3d03affa336
Brett Loewen <brettzky007@gmail.com>
2024-11-19 16:57:41 -0600

    Regatta form WIP

--- frontend/react/src/components/forms/RegattaForm.jsx
+++ frontend/react/src/components/forms/RegattaForm.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useRef} from "react";
 import { useNavigate } from 'react-router-dom';
 import Card from '../ui/Card.jsx';
+import getRaces from "../../utils/GetRaces.jsx";
 import PropTypes from "prop-types";
 import classes from "./Form.module.css";
 
@@ -11,15 +12,63 @@ RegattaForm.propTypes = {
 
 // Renders a form to create a new regatta
 export default function RegattaForm({onAddRegatta}) {
+    // Get all the available courses
+    const races = getRaces();
+
     // Used to navigate back to the homepage after submitting the regatta form
     const navigate = useNavigate();
 
     // References to the input fields to get their values
     const regattaTitleInputRef = useRef();
 
+    const [raceCount, setRaceCount] = useState(1);
+    const [raceChoices, setRaceChoices] = useState([]);
+
+    const handleRaceSelection = (event) => {
+        console.log(event.target.value);
+    }
+
+    const raceDropdowns = [];
+    if (races.length === 0) {
+        raceDropdowns.push(<p>Sorry, you haven't created any races yet</p>);
+    } else {
+        for(let i = 0; i < raceCount; i++) {
+            // Create a dropdown with all the courses as options, or just text if there are no courses available
+            const courseOptions = [];
+            races.map((race) => {
+                courseOptions.push(<option key={race.id} value={race.id}>{race.name}</option>)
+            });
+            const dropdown = (
+                    <select value={2} onChange={handleRaceSelection}>
+                        {courseOptions}
+                    </select>);
+    
+            raceDropdowns.push(
+                <Card key={i}>
+                    <div className={classes.control}>
+                        <div className={classes.mapbox}>
+                            <label htmlFor='course'>{`Race ${i + 1}`}</label>
+                            {dropdown}
+                        </div>
+                    </div>
+                </Card>
+            );
+        }
+    }
+
+    // Function used to update state so a new marker will be rendered in the form
+    function addRace() {
+        setRaceCount(raceCount + 1)
+    };
+
     // Pass (after formatting) the data from the form to the parent component
     function submitHandler(event) {
         event.preventDefault();
+
+        // Don't submit the form if there are no races available
+        if(races.length === 0) {
+            return;
+        }
 
         const regattaTitle = regattaTitleInputRef.current.value;
 
@@ -44,9 +93,14 @@ export default function RegattaForm({onAddRegatta}) {
                     <label htmlFor='title'>Regatta Title</label>
                     <input type='text' required id='title' ref={regattaTitleInputRef}/>
                 </div>
-                <div className={classes.actions}>
-                    <button>Create</button>
-                </div>
+                {raceDropdowns}
+                { /* Only render the submit button if there are available races */
+                    races.length > 0 &&
+                    <div className={classes.actions}>
+                        <button type="button" onClick={addRace}>Add Race</button>
+                        <button>Create</button>
+                    </div>
+                }
             </form>
         </Card>
     );
